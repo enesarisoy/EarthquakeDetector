724a30b0787e7e31f11f277e463bad15356b3340
enesarisoy <70036852+enesarisoy@users.noreply.github.com>
2023-10-02 15:24:15 +0300

    Delete app/src/main/java/com/ns/earthquakealert/Constants.kt

--- app/src/main/java/com/ns/earthquakealert/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.ns.earthquakealert
-
-object Constants {
-    const val BASE_URL = "https://fcm.googleapis.com"
-    const val SERVER_KEY = "AAAAq9YIemI:APA91bG-wJqtqMKCrq-wZ3Yhrj2fN4bAGrDuf2ereFK3_eMm9F2I6-ttXmg-7AA6dPgdT8L5egJCthyDgt4pk3rS0mvq2qrYvHokMRgxQsiDW1bkTUgVSfsk2IKgAiIxx0gt53cGCXoa"
-    const val CONTENT_TYPE = "application/json"
-}
